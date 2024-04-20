23083917346689105bbb3c5651fca4c9d5e7993e
salysaly <anyawee.sr@gmail.com>
2024-04-20 22:17:21 +0700

    get user input (useState, useRef, FormData)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import Header from './components/Header.jsx';
-import Login from './components/Login.jsx';
+import FormDataLogin from "./components/FormDataLogin.jsx";
+import Header from "./components/Header.jsx";
+import Login from "./components/Login.jsx";
+import RefLogin from "./components/RefLogin.jsx";
+import StateLogin from "./components/StateLogin.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Login />
+        {/* <Login /> */}
+        {/* <StateLogin /> */}
+        {/* <RefLogin /> */}
+        <FormDataLogin />
       </main>
     </>
   );
---
+++ src/components/StateLogin.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+
+export default function StateLogin() {
+  // const [didEdit, setDidEdit] = useState({
+  //   email: false,
+  //   password: false,
+  // });
+
+  const [enteredValue, setEnteredValue] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleBlurInput = (key) => {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [key]: true,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(enteredValue);
+  };
+
+  const handleInputChange = (key, value) => {
+    setEnteredValue((prevEntered) => ({
+      ...prevEntered,
+      [key]: value,
+    }));
+    // setDidEdit((prevEdit) => ({
+    //   ...prevEdit,
+    //   [key]: false,
+    // }));
+  };
+
+  const emailIsInValid =
+    enteredValue.email && !enteredValue.email.includes("@");
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>State Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onBlur={() => handleBlurInput("email")}
+            onChange={(e) => handleInputChange("email", e.target.value)}
+            value={enteredValue.email}
+          />
+          <div className="control-error">
+            {emailIsInValid && <p>please enter a valid email address</p>}
+          </div>
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(e) => handleInputChange("password", e.target.value)}
+            value={enteredValue.password}
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
